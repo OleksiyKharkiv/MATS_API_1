0cb45377accb218fe55e4e721e9b22d3d97da1f9
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-04-24 10:26:01 +0200

    Controller & Service 1.3 Run success. Postman 1 bug: PUT. Swagger ok

--- src/main/java/com/games/dndBE/controller/GameCharacterController.java
+++ src/main/java/com/games/dndBE/controller/GameCharacterController.java
@@ -2,8 +2,8 @@ package com.games.dndBE.controller;
 
 import com.games.dndBE.model.GameCharacter;
 import com.games.dndBE.service.GameCharacterService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,36 +12,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/gamecharacters")
 @RequiredArgsConstructor
-@Api(value = "Game Character API", tags = "Game Characters")
+//@Api(value = "Game Character API", tags = "Game Characters")
 public class GameCharacterController {
 
     private final GameCharacterService gameCharacterService;
 
-    @ApiOperation(value = "Get all game characters")
+//    @ApiOperation(value = "Get all game characters")
     @GetMapping
     public List<GameCharacter> getAllGameCharacters() {
         return gameCharacterService.getAllGameCharacters();
     }
 
-    @ApiOperation(value = "Get game character by ID")
+//    @ApiOperation(value = "Get game character by ID")
     @GetMapping("/{id}")
     public GameCharacter getGameCharacterById(@PathVariable String id) {
         return gameCharacterService.getGameCharacterById(id);
     }
 
-    @ApiOperation(value = "Create a new game character")
+//    @ApiOperation(value = "Create a new game character")
     @PostMapping
     public GameCharacter createGameCharacter(@RequestBody GameCharacter gameCharacter) {
         return gameCharacterService.createGameCharacter(gameCharacter);
     }
 
-    @ApiOperation(value = "Update an existing game character")
+//    @ApiOperation(value = "Update an existing game character")
     @PutMapping("/{id}")
     public GameCharacter updateGameCharacter(@PathVariable String id, @RequestBody GameCharacter gameCharacter) {
         return gameCharacterService.updateGameCharacter(id, gameCharacter);
     }
 
-    @ApiOperation(value = "Delete a game character by ID")
+//    @ApiOperation(value = "Delete a game character by ID")
     @DeleteMapping("/{id}")
     public void deleteGameCharacter(@PathVariable String id) {
         gameCharacterService.deleteGameCharacter(id);

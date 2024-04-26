6861cfa3847a0e70c3e6b32f5b6cf448553473ff
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-04-26 17:01:39 +0200

    Controller & Service 1.6 Run success. Swagger ok. Clear.

--- src/main/java/com/games/dndBE/DndApplication.java
+++ src/main/java/com/games/dndBE/DndApplication.java
@@ -2,7 +2,6 @@ package com.games.dndBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 //@EnableMongoRepositories
